371ae83160b5a00d7dc8e310b3808558f57d84b4
marvinody <manny@sadpanda.moe>
2019-03-10 06:03:47 -0400

    large update that may or may not work. Switched to using main api they use but had to decode the flag system they use

--- amiami/amiami.py
+++ amiami/amiami.py
@@ -4,82 +4,20 @@ from math import ceil
 import re
 from urllib.parse import urlencode
 
-rootURL = "https://www.amiami.com"
-class SimpleItem:
+rootURL = "https://api.amiami.com/api/v1.0/items"
+PER_PAGE = 30
+def removeSuffix(str, suffix):
+    if str.endswith(suffix):
+        return str[: -1*len(suffix)]
+
+class Item:
     def __init__(self, *args, **kwargs):
         self.productURL = kwargs['productURL']
         self.imageURL = kwargs['imageURL']
         self.productName = kwargs['productName']
         self.price = kwargs['price']
         self.productCode = kwargs['productCode']
-
-    @staticmethod
-    def parse(product_box_soup: element.Tag):
-        data = {
-            'productURL': "{}{}".format(rootURL, product_box_soup.find('a')['href']),
-            'productCode': product_box_soup.find('a')['href'],
-            'imageURL': product_box_soup.find('img')['src'],
-            'productName': product_box_soup.find("p",class_="newly-added-items__item__name").text,
-            # for some reason, it has a bunch of tabs everywhere for spacing
-            'price': product_box_soup.find("p", class_="newly-added-items__item__price").text.strip(),
-        }
-        data['productCode'] = re.search('gcode=(.*)', data['productCode']).group(1)
-        # process some stuff
-        data['price'] = re.search("(.*)[^,\d]", data['price']).group(1)
-        return SimpleItem(**data)
-
-def removeSuffix(str, suffix):
-    if str.endswith(suffix):
-        return str[: -1*len(suffix)]
-
-class Item(SimpleItem):
-    def __init__(self, *args, **kwargs):
-        super().__init__(**kwargs)
         self.availability = kwargs['availability']
-        self.about = kwargs['about']
-
-    @staticmethod
-    def use(simpleItem: SimpleItem):
-        MAX_ATTEMPTS = 10
-        currAttempt = 0
-        while currAttempt < MAX_ATTEMPTS:
-            try:
-                url = simpleItem.productURL
-                data = vars(simpleItem)
-                data['about'] = {}
-                r = requests.get(RENDER_URL,
-                    params={
-                        'url':url,
-                        'wait':3})
-                soup = BeautifulSoup(r.text, 'html.parser')
-
-                data['productName'] = soup.find('h2', class_='item-detail__section-title').text
-
-                def shown_btn_cart(tag):
-                    return "btn-cart" in tag['class'] and tag['style'] != "display: none;"
-
-                a = soup.find("div", class_="item-detail__operation__inner").find(shown_btn_cart)
-                data['availability'] = a.text
-
-                abouts = soup.find("section", class_="item-about").find_all("dt", class_="item-about__data-title")
-                for titleTag in abouts:
-                    sib = titleTag.next_sibling
-                    # skip if it doesn't have a corresponding text, should never happen
-                    if 'item-about__data-text' not in sib['class']:
-                        continue
-                    key = titleTag.text.replace(' ', '')
-                    val = sib.text
-                    data['about'][key] = val
-
-                return Item(**data)
-            except AttributeError as e:
-                #print(soup)
-                currAttempt += 1
-                print("Retrying page load, failed {} time(s)".format(currAttempt))
-                continue
-        else:
-            raise AttributeError("Could not successfully generate info")
-
 
 class ResultSet:
 
@@ -90,53 +28,60 @@ class ResultSet:
         self.pages = -1
         self._itemCount = 0
 
-    def get(self, idx: int) -> Item:
-        item = self.items[idx]
-        if isinstance(item, Item):
-            return item # dont need to refetch the data
-        self.items[idx] = Item.use(item)
-        return self.items[idx]
+    def add(self, productInfo):
+        inStock = productInfo['instock_flg'] == 1
+        isClosed = productInfo['order_closed_flg'] == 1
+        isPreorder = productInfo['preorderitem'] == 1
+        isBackorder = productInfo['list_backorder_available'] == 1
+        if isClosed:
+            if isPreorder:
+                availability = "Pre-order Closed"
+            elif isBackorder:
+                availability = "Back-order Closed"
+            else:
+                availability = "Order Closed"
+        elif isBackorder:
+            availability = "Back-order"
+        else:
+            if isPreorder and inStock:
+                availability = "Pre-order"
+            elif inStock:
+                availability = "Available"
+        item = Item(
+            productURL="https://www.amiami.com/eng/detail/?gcode={}".format(productInfo['gcode']),
+            imageURL="https://img.amiami.com{}".format(productInfo['thumb_url']),
+            productName=productInfo['gname'],
+            price=productInfo['c_price_taxed'],
+            productCode=productInfo['gcode'],
+            availability=availability,
+        )
+        self.items.append(item)
 
-    def add(self, product_box_soup):
-        self.items.append(SimpleItem.parse(product_box_soup))
-
-    def parse(self, html):
-    # returns true when done
-    # false if can be called again
-        soup = BeautifulSoup(html, 'html.parser')
+    def parse(self, obj):
+        # returns true when done
+        # false if can be called again
         if not self.init:
-            countElm = soup.find('p', class_='search-result__text')
-            if not countElm:
-                self.maxItems = 0
-                self.pages = 0
-                self.init = True
-                return True
-            # countElm.text := "81-100 of 3377 results" or something
-            self.maxItems = int(re.search('of (\d+)', countElm.text).group(1))
+            self.maxItems = obj['search_result']['total_results']
             self.pages = int(ceil(self.maxItems / float(PER_PAGE)))
             self.init = True
-        for product_box in soup.find_all('li', class_='newly-added-items__item'):
-            self.add(product_box)
+        for productInfo in obj['items']:
+            self.add(productInfo)
             self._itemCount += 1
 
         return self._itemCount == self.maxItems
 
-# 172... is the local ip of docker service containing splash service
-# wait about 5s to be sure that everything loaded
-# need to make this dynamic eventually and allow ip to be passed
-RENDER_URL = "http://172.17.0.1:8080/render.html"
-PER_PAGE = 20 # this is hardcoded and not really used tho
 def search(keywords: str) -> ResultSet:
-    print("hell1o")
     data = {
         "s_keywords": keywords,
         "pagecnt": 1,
+        "pagemax": PER_PAGE,
+        "lang": "eng",
+    }
+    headers = {
+        "X-User-Key": "amiami_dev"
     }
     rs = ResultSet()
-    while not rs.parse(requests.get(RENDER_URL,
-        params={
-            'url':'https://www.amiami.com/eng/search/list?{}'.format(urlencode(data)),
-            'wait':5}).text):
-
+    while not rs.parse(requests.get(rootURL, data, headers=headers).json()):
         data['pagecnt'] += 1
+
     return rs
